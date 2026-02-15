8c38e05eed524a6fb768d2813848e6d56b7e4793
diogoabreu <134980133+diogooabreu@users.noreply.github.com>
2026-02-14 22:26:17 -0300

    Exercices list two (#1)
    * List two
    
    * Finished list two
    
    * Added tests for list two

---
+++ exercices-list-two/src/index.ts
@@ -0,0 +1,11 @@
+import Logic from "./Logic.js";
+
+const Testing = new Logic();
+
+console.log(Testing.calcSquareMeasurements(10));
+console.log(Testing.calcCircleArea(10));
+console.log(Testing.calcTriangleMeasurements(10, 5, 7));
+console.log(Testing.successor(15))
+console.log(Testing.division(15, 40))
+console.log(Testing.calcAge(6314));
+console.log(Testing.convert(570));
